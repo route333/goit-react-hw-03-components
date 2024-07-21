e11ba0d4dc57db377f55040a9d1e57f0f799067c
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2024-07-21 21:40:28 +0300

    q

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
     <>
       <div class="profile">
         <div class="description">
-          <img src={avatar} alt="User avatar" class="avatar" />
+          <img src={avatar} alt="Usr avatar" class="avatar" />
           <p class="name">{username}</p>
           <p class="tag">@{tag}</p>
           <p class="location">{location}</p>
